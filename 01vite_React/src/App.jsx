a63dcc29bcf62ab770bf48c8c6a5994fb461deea
Pradeep123-sharma <pradeep131006@gmail.com>
2025-09-03 14:44:44 +0530

    Initialize Vite React project with essential configuration and components

---
+++ 01vite_React/src/App.jsx
@@ -0,0 +1,10 @@
+/* Now we can delete some files and folders. We'll delete assets folder, App.css, index.css files from src folder.
+ */
+function App() {
+
+  return (
+    <h1>Chai aur react with vite</h1>
+  )
+}
+
+export default App
