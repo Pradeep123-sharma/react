63484eb1a76d1da7d7f865cab3b1c1a4f7c9e85b
Pradeep123-sharma <pradeep131006@gmail.com>
2025-10-27 14:03:46 +0530

    RTK todo project part-2

--- 10reduxToolkitToDo/src/features/todo/todoSlice.js
+++ 10reduxToolkitToDo/src/features/todo/todoSlice.js
@@ -11,8 +11,25 @@ export const todoSlice = createSlice({
     /* We provide initial state. Ab jo humne pehle initial state likhi thi vo hum yha par bhi de skte hai ya seedhe hi variable ka reference de skte hai. */
     initialState,
     // Writing reducers
-    reducers: {
-        addTodo: (state, action) => {},
-        removeTodo: () => {}
+    reducer: {
+        addTodo: (state, action) => {
+            // Creating todo
+            const todo= {
+                id: nanoid(),
+                text: action.payload
+            }
+            
+            // Changing the state
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        }
     }
 })
+
+// Exporting individual reducer methods
+export  const { addTodo, removeTodo } = todoSlice.actions
+
+// Exporting reducers
+export default todoSlice.reducer
