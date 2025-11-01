1aacf3c272eeaa855ffb7e9be8bbe8036788a0ce
Pradeep123-sharma <pradeep131006@gmail.com>
2025-11-01 13:33:51 +0530

    Mega Blog Project Part-2 Made Services file

---
+++ 12MegaBlogProject/src/appwrite/auth.js
@@ -0,0 +1,72 @@
+import config from '../config/config.js'
+import { Client, Account, ID } from "appwrite"
+
+/* So this is our future proof code where even if we use any other services in future then we make changes only and only in this file. We don't have to change frontend part. */
+export class AuthService {
+    client = new Client();
+    account;
+
+    /* To humne yaha par constructor isliye banaya hai kyunki jab koi bhi user is object ko banaye tab uska client banna chahiye aur fir properly uska account bane. */
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+         this.account = new Account(this.client)
+    }
+
+    /* 
+        So we'll make a method 'createAccount' which under the hood implements the logic for appwrite but we don't  want to show to users, so user will simply call this method to create account but bts he don't knows which service logic we are using.
+        So actually inside we are implementing the appwrite logic but if we want to change the service(eg- firebase) in future we can simply change the logic inside this method and constructor without changing anything outside. So this is the main purpose of service layer.
+    */
+    async createAccount(email, password, name) {
+        try {
+            const userAccount = await this.account.create(
+                ID.unique(),
+                email,
+                password,
+                name,
+            )
+            if (userAccount) {
+                // We''ll call another method of login.
+                return this.login(email, password)
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login(email, password) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getAccount() {
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error = ", error);
+        }
+
+        // If any error occurs in even try catch block also, then also it returns null.
+        return null
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error = ", error);
+        }
+    }
+
+}
+
+// Here 'authService' is an object.
+const authService = new AuthService()
+
+export default AuthService
---
+++ 12MegaBlogProject/src/appwrite/service.js
@@ -0,0 +1,136 @@
+import config from "../config/config.js";
+import { Client, ID, TablesDB, Storage, Query } from "appwrite";
+
+export class Service {
+    client = new Client();
+    tablesDB;
+    storage;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.tablesDB = new TablesDB(this.client);
+        this.storage = new Storage(this.client);
+    }
+
+    async createPost(title, slug, featuredImage, content, status, userId) {
+        try {
+            return await this.tablesDB.createRow(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug, // row ID
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error = ", error);
+        }
+    }
+
+    async updatPost(slug, {title, featuredImage, content, status}) {
+        try {
+            return await this.tablesDB.updateRow(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: updatPost :: error = ", error);
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.tablesDB.deleteRow(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error = ", error);
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.tablesDB.getRow(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error = ", error);
+            return false
+        }
+    }
+
+    /* 
+        So here in this fun 'queries' is a variable name and real query is written in square brackets. We can pass multiple queries as an array.
+        So here we are passing a default query to get only active posts. We can filter posts on basise of status because we have defined status as an index in our table.
+        We can only do queries if we have defined indexes in our table. Here 'status' is a key.
+    */
+    async getAllPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.tablesDB.listRows(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getAllPosts :: error = ", error);
+            return false
+        }
+    }
+
+// ****************************************************************************************************************
+    // FILE UPLOADING SERVICES
+    async uploadFile(file) {
+        try {
+            return await this.storage.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error = ", error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.storage.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            );
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error = ", error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.storage.getFilePreview(
+            config.appwriteBucketId,
+            fileId
+        )
+    }
+}
+
+const service = new Service();
+
+export default service
